3a3571dc1fe00991adc766f1bb4dfb997552a026
Adil Sadik <sparky.005@gmail.com>
2018-05-23 21:47:50 -0400

    should be working

--- backup.sh
+++ backup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 export REPO='/home/asadik/.borg_backup'
+export BORG_PASSPHRASE=''
+
 
 # die if borg is already running
 if pidof -x borg >/dev/null; then
@@ -9,7 +11,7 @@ if pidof -x borg >/dev/null; then
 fi
 
 borg create --stats -v                      \
-    $REPO::'{hostname}-{now:%Y-%m-%d}-1'      \
+    $REPO::'{hostname}-{now:%Y-%m-%d}'      \
     /home/asadik                            \
     --exclude '/home/asadik/.borg_backup'   \
     --exclude '/home/asadik/.local'         \
@@ -18,3 +20,15 @@ borg create --stats -v                      \
 # prune to remove old backups
 borg prune -v --stats --list $REPO --prefix '{hostname}-' \
     --keep-daily=7 --keep-weekly=4 --keep-monthly=6
+
+
+# check if we're on my wifi network
+#networkname=$(nmcli -t -f active,ssid dev wifi | cut -f2 -d':' | head -1)
+networkname=$(iwgetid -r)
+if [ "$networkname" == '715 - CR∑∑KS' ]; then
+    # we're tethered to my phone
+    # bail
+    echo "Connected to phone, not uploading to aws"
+    exit
+fi
+aws s3 sync $REPO s3://peterpanda2-backups --storage-class STANDARD_IA
